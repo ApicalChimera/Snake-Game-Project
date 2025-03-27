e58009bf4f6d78f7c220a32e07578047fc5928ae
Brian Dietz <briandietz888@gmail.com>
2025-03-27 11:43:46 -0400

    tt

--- snake2/snake2.cpp
+++ snake2/snake2.cpp
@@ -1,7 +1,7 @@
 /*
 * Snake Game Project
 * By: Eleek Zhang, Daniel Chen, Sean Lin, & Brian Dietz
-* Due Date: April 3rd, 2029
+* Due Date: April 3rd, 2025
 */
 #include<iostream>
 #include<windows.h>
